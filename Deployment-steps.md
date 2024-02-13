96440a83111c68b8ad83e5196493d4ae479bd49e
Burak Orkmez <burakorkmezz@gmail.com>
2024-02-14 02:18:06 +0300

    deployment-steps.md file added

---
+++ Deployment-steps.md
@@ -0,0 +1,13 @@
+# SETUP server.js (express.static and res.sendFile for react app)
+
+# SETUP "build" and "start" scripts in package.json
+
+# Deploy on render.com with env variables
+
+# Update Github OAuth App (homepage url and callback url)
+
+# Add CLIENT_BASE_URL as env variable on render
+
+# Update the callback url on github.auth.js to full url (deployed url)
+
+# Lastly, subscribe to make it work ;)
--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-console.log("dirname", __dirname);
-
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors());
+
+// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
+// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
