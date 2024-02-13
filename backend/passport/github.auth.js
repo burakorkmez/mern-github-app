1617466e22b33d18e4ec5ee866037b748b5b2cab
Burak Orkmez <burakorkmezz@gmail.com>
2024-02-13 15:29:47 +0300

    Callback url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "/api/auth/github/callback",
+			callbackURL: "https://mern-github-app.onrender.com/api/auth/github/callback",
 		},
 		async function (accessToken, refreshToken, profile, done) {
 			const user = await User.findOne({ username: profile.username });
